19ff64c72151f5b97e659d24434b28c6a21d9631
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-08-04 01:44:02 +0500

    Updated app.js

--- app.js
+++ app.js
@@ -6,3 +6,13 @@
 // var bool = [true, false];
 // var mixed = ["mobile", 2, true];
 // var qualification = ["SSC", "HSC", "BCS","BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+// document.write("Qualification :" + "<br>","<br>");
+// document.write("1) " + qualification[0] + "<br>");
+// document.write("2) " + qualification[1] + "<br>");
+// document.write("3) " + qualification[2] + "<br>");
+// document.write("4) " + qualification[3] + "<br>");
+// document.write("5) " + qualification[4] + "<br>");
+// document.write("6) " + qualification[5] + "<br>");
+// document.write("7) " + qualification[6] + "<br>");
+// document.write("8) " + qualification[7] + "<br>","<br>");
