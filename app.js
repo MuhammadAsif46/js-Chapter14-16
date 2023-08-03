e57d2e055ac139e49cef1dc4997e2fb50006f3d0
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-08-04 01:43:23 +0500

    Updated app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+//******** Declare and initialize studentName,number,string,boolean,mixed & Qualification ************/
+
+// var stdNames = ["asif", "ahmed", "kashan"];
+// var number = [1, 2, 3, 4, 6, 7, 5, 9];
+// var string = ["apple", "mango", "orange"];
+// var bool = [true, false];
+// var mixed = ["mobile", 2, true];
+// var qualification = ["SSC", "HSC", "BCS","BS", "BCOM", "MS", "M. Phil.", "PhD"];
